99a4fd21209c65a6b8422b7cf920ccb2ae6af7bf
Onur Casun <onurcasun@outlook.com>
2021-06-04 19:40:20 +0300

    first commit

---
+++ limits-service/src/main/java/com/onurcasun/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,19 @@
+package com.onurcasun.microservices.limitsservice.controller;
+
+import com.onurcasun.microservices.limitsservice.bean.Limits;
+import com.onurcasun.microservices.limitsservice.configuration.Configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+    @GetMapping("/limits")
+    public Limits retrieveLimits(){
+        return new Limits(configuration.getMinimum(),configuration.getMaximum());
+    }
+}
