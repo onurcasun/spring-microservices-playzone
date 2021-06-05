fa35797781a154888dd3f11053d644b8e5c20bed
Onur Casun <onurcasun@outlook.com>
2021-06-05 16:59:23 +0300

    (step 13) - JPA configured for data initialization

---
+++ currency-exchange-microservice/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment) 
+values(10001, 'USD', 'TR', 8.67, '');
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment) 
+values(10002, 'EUR', 'TR', 10.55, '');
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment) 
+values(10003, 'RUB', 'TR', 0.12, '');
