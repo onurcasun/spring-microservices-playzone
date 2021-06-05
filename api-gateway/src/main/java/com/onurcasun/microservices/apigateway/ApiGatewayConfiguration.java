cba617ee72cafd7bf8ab1d08fed0fa220732c998
Onur Casun <onurcasun@outlook.com>
2021-06-05 20:44:10 +0300

    api gateway impl

---
+++ api-gateway/src/main/java/com/onurcasun/microservices/apigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,31 @@
+package com.onurcasun.microservices.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+
+        // Example of adding request and parameter to header
+        // return builder.routes()
+        //          .route( p -> p.path("/get") .filters(f ->
+        //              f.addRequestHeader("MyHeader", "MyURI").addRequestParameter("Param",
+        //              "Value")) .uri("http://httpbin.org:80")).build()
+
+        return builder.routes().route(p -> p.path("/currency-exchange/**").uri("lb://currency-exchange"))
+                .route(p -> p.path("/currency-conversion/**").uri("lb://currency-conversion"))
+                .route(p -> p.path("/currency-conversion-feign/**").uri("lb://currency-conversion"))
+                .route(p -> p.path("/currency-conversion-new/**")
+                        .filters(f -> f.rewritePath("/currency-conversion-new/(?<segment>.*)",
+                                "/currency-conversion-feign/${segment}"))
+                        .uri("lb://currency-conversion"))
+                .build();
+    }
+
+}
