52d15d7bb7871a5108a0c5aa249e8fe17dc77034
Onur Casun <onurcasun@outlook.com>
2021-06-07 18:41:36 +0300

    dependencies added for distributed tracing

--- currency-exchange-microservice/src/main/java/com/onurcasun/microservices/currencyexchangemicroservice/CircuitBreakerController.java
+++ currency-exchange-microservice/src/main/java/com/onurcasun/microservices/currencyexchangemicroservice/CircuitBreakerController.java
@@ -2,14 +2,14 @@ package com.onurcasun.microservices.currencyexchangemicroservice;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
+// import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+// import org.springframework.web.client.RestTemplate;
 
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+// import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+// import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @RestController
 public class CircuitBreakerController {
